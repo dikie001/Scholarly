bed7d317165dad5765b243653710f2ed1a13a028
dikie001 <omondidickens255@gmail.com>
2025-10-09 18:07:43 +0300

    `Refactored Login page layout and components`

--- client/src/pages/Auth/Login.tsx
+++ client/src/pages/Auth/Login.tsx
@@ -1,14 +1,13 @@
-import {
-  Card,
-  CardHeader,
-  CardTitle,
-  CardContent,
-  CardFooter,
-  CardDescription,
-} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import React, { useState } from "react";
+import { useState } from "react";
 
 const Login = () => {
   const [admissionNumberExist, setAdmissionNumberExist] = useState(false);
@@ -19,10 +18,14 @@ const Login = () => {
   return (
     <div className="h-screen w-full flex justify-center items-center  p-6">
       <Card className="w-full max-w-xl shadow-lg p-4">
-        <div className="flex justify-between gap-4">
-          <div>
-            
-            <div className="mt-12">
+        <CardTitle className="text-4xl sm:text-5xl flex items-center mx-auto font-bold text-blue-600 max-sm:-mb-2">
+          Scholarly <sup className="text-xs ">TM</sup>
+        </CardTitle>
+        <div className="flex sm:justify-between gap-4">
+        
+          <div className="max-sm:hidden">
+            {/* Instrucations for students and faculty */}
+            <div className="mt-2">
               <p>
                 <span className="font-medium text-sm">Students:</span>{" "}
                 <span className="text-sm text-muted-foreground">
@@ -39,20 +42,21 @@ const Login = () => {
                 </span>
               </p>
 
-              <a href="contact_support" className="text-[14px] text-blue-600">Contact support</a>
+              <a href="contact_support" className="text-[14px] text-blue-600">
+                Contact support
+              </a>
+            </div>
+
+            {/* Padlock image */}
+            <div className="mt-4">
+              <img src="/images/padlock.png" width={100} alt="" />
             </div>
           </div>
-          <div className="max-w-sm ">
-            {" "}
+          <div className="max-w-sm max-sm:mx-auto ">
+            <img src="/images/padlock.png" width={100} className="sm:hidden mx-auto mb-2" alt="padlock" />
             <CardHeader className="text-center">
-              <CardTitle className="text-3xl md:text-4xl font-bold text-blue-600">
-                Scholarly <sup className="text-xs ">TM</sup>
-              </CardTitle>
-              {/* <CardDescription className="text-muted-foreground">
-                Sign in to continue
-              </CardDescription> */}
             </CardHeader>
-            <CardContent className="space-y-4 border-2  rounded-xl p-4">
+            <CardContent className="space-y-4  border-2  rounded-xl p-4">
               <div>
                 <label htmlFor="email" className="text-sm font-medium">
                   Admission number
@@ -81,7 +85,7 @@ const Login = () => {
 
               <Button
                 onClick={HandleAuthentication}
-                className="w-full bg-blue-600 hover:bg-blue-700  max-w-xs mx-auto flex sm:w-50"
+                className="w-full bg-gradient-to-l from-blue-600 to-blue-400  max-w-xs mx-auto flex max-sm:w-60 sm:w-50"
               >
                 {admissionNumberExist ? "Login" : "Authenticate"}
               </Button>
