0c959673ff6e709bf0c8a8ae71fd3dabd3e89551
dikie001 <omondidickens255@gmail.com>
2025-10-10 01:47:11 +0300

    feat: add dropdown menu component and integrate account type selection in signup

--- client/src/pages/Auth/Signup.tsx
+++ client/src/pages/Auth/Signup.tsx
@@ -1,14 +1,45 @@
 import { Button } from "@/components/ui/button";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardTitle
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import React, { useState } from "react";
+import type { accountTypes, userTypes } from "@/types/auth";
 
 const Signup = () => {
+  const [accountType, setAccountType] = useState<accountTypes>("");
+  const [user, setUser] = useState<userTypes>({
+    fullName: "",
+    email: "",
+    password: "",
+    admissionNumber: "",
+    phoneNumber: "",
+    accountType: "",
+  });
+
+  // Handle user info, update the object
+  const HandleUser = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  // Handle account type selection
+  const HandleAccountType = (params: accountTypes) => {
+    setAccountType(params);
+    setUser((prev) => ({ ...prev, accountType: params }));
+  };
   return (
     <div className="min-h-screen w-full flex justify-center items-center sm:p-6 ">
       <div className="border relative max-w-2xl w-full p-6 py-24 rounded-xl">
@@ -21,7 +52,9 @@ const Signup = () => {
           <CardTitle className="text-4xl sm:text-5xl flex items-center mx-auto font-bold text-blue-600 max-sm:-mb-2">
             Scholarly <sup className="text-xs">TM</sup>
           </CardTitle>{" "}
-          <CardDescription className="mx-auto -mt-2 -mb-2 sm:hidden">Fill in your details</CardDescription>
+          <CardDescription className="mx-auto -mt-2 -mb-2 sm:hidden">
+            Fill in your details
+          </CardDescription>
           <div className="flex sm:justify-between gap-4">
             <div className="max-sm:hidden">
               <div className="mt-20">
@@ -34,8 +67,42 @@ const Signup = () => {
               </div>
             </div>
             <div className="max-w-sm max-sm:mx-auto">
-            
               <CardContent className="space-y-4 border-2 rounded-xl p-4">
+                {/* Account type */}
+                <label className="text-sm font-medium">Account type</label>
+                <DropdownMenu>
+                  <DropdownMenuTrigger
+                    className={`border cursor-pointer  flex  w-full py-1.5 px-4 rounded-lg ${
+                      accountType === "" && "text-gray-500"
+                    }`}
+                  >
+                    {accountType || "Select role"}
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent>
+                    <DropdownMenuLabel>Select role</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem
+                      className="cursor-pointer"
+                      onClick={() => HandleAccountType("student")}
+                    >
+                      Student
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                      className="cursor-pointer"
+                      onClick={() => HandleAccountType("faculty")}
+                    >
+                      Faculty
+                    </DropdownMenuItem>
+                    <DropdownMenuItem
+                      className="cursor-pointer"
+                      onClick={() => HandleAccountType("parent")}
+                    >
+                      Parent
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+
+                {/* Full name */}
                 <div>
                   <label htmlFor="fullName" className="text-sm font-medium">
                     Full Name
@@ -43,43 +110,76 @@ const Signup = () => {
                   <Input
                     id="fullName"
                     type="text"
+                    name="fullName"
+                    onChange={(e) => HandleUser(e)}
                     placeholder="John doe"
                     className="mt-1"
                   />
                 </div>
-    
-        
-                <div>
-                  <label htmlFor="email" className="text-sm font-medium">
-                    Email
-                  </label>
-                  <Input
-                    id="email"
-                    type="email"
-                    placeholder="john.doe@example.com"
-                    className="mt-1"
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="admissionNumber"
-                    className="text-sm font-medium"
-                  >
-                    Admission Number
-                  </label>
-                  <Input
-                    id="admissionNumber"
-                    type="number"
-                    placeholder="14572"
-                    className="mt-1"
-                  />
-                </div>
+
+                {accountType === "faculty" && (
+                  <div>
+                    <label htmlFor="email" className="text-sm font-medium">
+                      Email
+                    </label>
+                    <Input
+                      id="email"
+                      type="email"
+                      onChange={(e) => HandleUser(e)}
+                      name="email"
+                      placeholder="john.doe@example.com"
+                      className="mt-1"
+                    />
+                  </div>
+                )}
+
+                {/* Admission Number */}
+                {accountType === "student" && (
+                  <div>
+                    <label
+                      htmlFor="admissionNumber"
+                      className="text-sm font-medium"
+                    >
+                      Admission Number
+                    </label>
+                    <Input
+                      id="admissionNumber"
+                      type="number"
+                      onChange={(e) => HandleUser(e)}
+                      name="admissionNumber"
+                      placeholder="14572"
+                      className="mt-1"
+                    />
+                  </div>
+                )}
+
+                {/* Phone Number */}
+                {(accountType === "parent" || accountType === "faculty") && (
+                  <div>
+                    <label
+                      htmlFor="phoneNumber"
+                      className="text-sm font-medium"
+                    >
+                      Phone Number
+                    </label>
+                    <Input
+                      id="phoneNumber"
+                      type="number"
+                      onChange={(e) => HandleUser(e)}
+                      name="phoneNumber"
+                      placeholder="0716952782"
+                      className="mt-1"
+                    />
+                  </div>
+                )}
                 <div>
                   <label htmlFor="password" className="text-sm font-medium">
                     Password
                   </label>
                   <Input
                     id="password"
+                    name="password"
+                    onChange={(e) => HandleUser(e)}
                     type="password"
                     placeholder="••••••••"
                     className="mt-1"
--- client/src/types/auth.ts
+++ client/src/types/auth.ts
@@ -1,3 +1,12 @@
+export type accountTypes = "student" | "faculty" | "parent" | "";
+export type userTypes = {
+  fullName: string;
+  email: string;
+  accountType: accountTypes;
+  admissionNumber?: string;
+  password: string;
+  phoneNumber: string;
+};
 export type credentialsTypes = {
   identifier: string;
   password: string;
