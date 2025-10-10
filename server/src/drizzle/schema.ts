d68a8c5e007e021d870565251d63df886b094b3b
dikie001 <omondidickens255@gmail.com>
2025-10-10 18:50:19 +0300

    `Update database connection URL and schema in server code`

--- server/src/drizzle/schema.ts
+++ server/src/drizzle/schema.ts
@@ -1,7 +1,7 @@
-import { integer, pgTable, varchar, uuid } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
-  id: uuid("id").primaryKey().defaultRandom(),
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,17 +2,26 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { configDotenv } from "dotenv";
 
-
 const db = drizzle(process.env.DATABASE_URL!);
 
 import express from "express";
+import { usersTable } from "./drizzle/schema.js";
+import { uuid } from "drizzle-orm/pg-core";
 
 configDotenv();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+const user: typeof usersTable.$inferInsert = {
+  name: "John",
+  age: 30,
+  email: "john@example.com",
+};
+
+app.get("/", async (req, res) => {
   console.log("Server is live...");
+  const allUsers = await db.insert(usersTable).values(user);
+  console.log(allUsers);
 });
 
 app.listen(PORT, () => {
