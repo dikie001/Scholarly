6a44158c207b0f71c5eecbdcd78d3ed89de5ec7e
dikie001 <omondidickens255@gmail.com>
2025-10-10 15:30:28 +0300

    `feat: updated server configuration and dependencies`

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,20 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { configDotenv } from "dotenv";
+
 
 const db = drizzle(process.env.DATABASE_URL!);
+
+import express from "express";
+
+configDotenv();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  console.log("Server is live...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server live in http://localhost:${PORT}`);
+});
